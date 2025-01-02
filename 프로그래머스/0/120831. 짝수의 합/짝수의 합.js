7d3b6e5d58057e71c4d3342bd9ff98c6a3ffd5b8
JoYoungHyun <younghyun550@gmail.com>
2025-01-02 09:50:14 +0900

    [level 0] Title: 짝수의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    if(0 < n && n <= 1000) {
+        for(i = 0; i <= n; i++) {
+            if(i % 2 == 0) {
+                answer += i
+            }
+        }
+    }
+    return answer;
+}
