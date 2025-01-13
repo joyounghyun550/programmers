3bfb639448b9124e994654549e2bad9226849d29
JoYoungHyun <younghyun550@gmail.com>
2025-01-13 10:27:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+  if (num === 1) return 0;
+  var count = 1;
+  while (count <= 500) {
+    num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+    if (num === 1) return count;
+    else count++;
+  }
+  return -1;
+}
