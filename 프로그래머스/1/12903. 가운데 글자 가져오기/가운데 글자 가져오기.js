be9f6da76a534756b5f665de159cc1a2ea52d77a
JoYoungHyun <younghyun550@gmail.com>
2025-02-06 09:59:56 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,7 +1,7 @@
 function solution(s) {
-  const mid = Math.floor(s.length / 2);
-  return s.length % 2 === 0
-    ? s.slice(mid - 1, mid + 1)  // 짝수 길이일 때
-    : s[mid];  // 홀수 길이일 때
+    const mid = Math.floor(s.length / 2);
+    const result = s.length % 2 === 0
+    ? s.slice(mid - 1, mid + 1)
+    : s[mid];
+    return result;
 }
-
