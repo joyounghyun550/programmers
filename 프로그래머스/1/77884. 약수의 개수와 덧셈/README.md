36238a63f39da68b28965f8d55397e406da619ef
JoYoungHyun <younghyun550@gmail.com>
2025-02-07 10:18:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 6.69 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 4.75 ms
+메모리: 36.3 MB, 시간: 6.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 09:54:15
+2025년 02월 07일 10:18:42
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,23 +1,13 @@
 function solution(left, right) {
   let result = 0;
-  let count = [];
-  let total = [];
   for (let i = left; i <= right; i++) {
+      let count = 0;
     for (let j = 1; j <= i; j++) {
-      if (i % j === 0) {
-        count.push(j);
-      }
+      if (i % j === 0) count++
     }
-    total.push(count);
-    count = [];
-  }
-
-  for (item of total) {
-    if (item.length % 2 === 0) {
-      result += item[item.length - 1];
-    } else {
-      result -= item[item.length - 1];
-    }
+      count % 2
+      ? result -= i
+      : result += i
   }
   return result;
 }
